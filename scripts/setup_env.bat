cd1d16f9412292ac9d1347b6131e3a1d0af2a72b
Brady Kirkpatrick <bradydkirkpatrick@gmail.com>
2021-06-30 14:52:03 -0400

    Fix environment setup script

--- scripts/setup_env.bat
+++ scripts/setup_env.bat
@@ -3,9 +3,9 @@ REM Call vcvars64.bat to setup the build environment (i.e. cl.exe)
 
 REM Find vcvars64.bat file and run it
 IF NOT DEFINED DevEnvDir (
-    IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bats" (
+    IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat" (
         call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat"    
-    ) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bats" (
+    ) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
     ) ELSE (
         echo "Unable to find vcvars64.bat, make sure you have Visual Studio 2019 or later installed"
